17aacc258ef3801104d37dff27d4c3c20c05211f
EynarYmir <tubik30rus@gmail.com>
2021-04-06 14:37:28 +0300

    Добавлен корабль с возможностью перемещения и стрельбы

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,35 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
 def run_game():
     """Инициализирует игру и создает объект экрана."""
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion.")
     
-    # Запуск основного цикла игры
+    #Создание экзепляра корабля
+    ship = Ship(ai_settings,screen)
+    # Создание группы пуль
+    bullets = Group()
     
+    # Назначениие цвета фона.
+    bg_color = (230, 230, 230)
+    
+    # Запуск основного цикла игры
     while True:
-        """Отслеживание событий клавиатуры и мыши"""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        print(len(bullets))
+        # При каждом проходе цикла перерисовывается экран.
+        gf.update_screen(ai_settings, screen, ship, bullets)
         """Отображение последнего проприсованного экрана"""
         pygame.display.flip()
 
